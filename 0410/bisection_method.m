3f53d90f338c228bc7f9b8a3302e03be8bc55ff6
Kang HyoJun <admire9@gmail.com>
2014-04-09 17:43:04 +0900

    이분법 완성 ㅎㅋ

---
+++ 0410/bisection_method.m
@@ -0,0 +1,32 @@
+function [r, stop_i] = bisection_method(x1, x2, fname)
+  x3old = x1;
+
+  for i=1:1000
+    y1 = feval(fname, x1);
+    y2 = feval(fname, x2);
+    yflag = y1 * y2;
+
+    if yflag > 0
+      printf("f(x1) 과 f(x2) 의 부호가 같습니다.");
+      break;
+    end
+
+    x3 = (x1 + x2) / 2;
+    flag = feval(fname, x1) * feval(fname, x3);
+    rel_error = (abs(x3 - x3old) / x3) * 100;
+
+    if rel_error < 0.005
+      break;
+    end
+
+    if flag < 0
+      x2 = x3;
+    else
+      x1 = x3;
+    end
+    x3old = x3;
+  end
+
+  stop_i = i;
+  r = x3;
+end

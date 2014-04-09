701b5573049fec235cfe4d79e9ca5997d1511adc
Kang HyoJun <admire9@gmail.com>
2014-04-09 20:20:46 +0900

    고정법 반복법 만듬 #5

---
+++ 0410/fixed_pos.m
@@ -0,0 +1,16 @@
+function r = fixed_pos(x, fname)
+  old = x;
+
+  for i=1:1000
+    y1 = feval(fname, old);
+
+    if i ~=1 && rel_error(y1, old, 0.005)
+      break;
+    end
+
+    old = y1;
+  end
+
+  stop_i = i;
+  r = x3;
+end

cacfc3921b067c0699c02c6820d16451a874ec42
Kang HyoJun <admire9@gmail.com>
2014-04-09 23:56:49 +0900

    할선법 구현 close #7

--- 0410/secant.m
+++ 0410/secant.m
@@ -2,13 +2,17 @@ function [r, stop_i] = secant(x1, x2, fname)
   for i=1:1000
     y1 = feval(fname, x1);
     y2 = feval(fname, x2);
-    x3 = x2 - y2 * (x2 - x1) / (y2 - y1);
+
+    yquan = y2 - y1;
+    if yquan == 0
+      disp("f(x2) - f(x1) 의 값이 0 이되었습니다.");
+      break;
+    end
+    x3 = x2 - y2 * (x2 - x1) / yquan;
 
     if i ~=1 && rel_error(x3, x2, 0.005)
       break;
     end
-
-    [x1, x2] = choice_x(x1, x2, x3, fname);
 
     x1 = x2;
     x2 = x3;

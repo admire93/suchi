a1197a8584de502fb10fcc2d2426ece8116c47f9
Kang HyoJun <admire9@gmail.com>
2014-04-09 17:54:00 +0900

    가위치법 만듬

---
+++ 0410/false_position_method.m
@@ -0,0 +1,26 @@
+function [r, stop_i] = false_position_method(x1, x2, fname)
+  x3old = x1;
+
+  for i=1:1000
+    y1 = feval(fname, x1);
+    y2 = feval(fname, x2);
+    yflag = y1 * y2;
+
+    if yflag > 0
+      printf("f(x1) 과 f(x2) 의 부호가 같습니다.");
+      break;
+    end
+
+    x3 = ((y2 - y1) / (x2 - x1)) * -x1 + y1;
+
+    if i ~=1 && rel_error(x3, x3old, 0.005)
+      break;
+    end
+
+    [x1, x2] = choice_x(x1, x2, x3, fname);
+    x3old = x3;
+  end
+
+  stop_i = i;
+  r = x3;
+end

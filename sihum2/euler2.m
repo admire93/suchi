55cc5630bd207379a3dab8706b30b6507836e821
Kang HyoJun <admire9@gmail.com>
2014-06-17 21:02:05 +0900

    오일러

--- sihum2/euler2.m
+++ sihum2/euler2.m
@@ -3,4 +3,21 @@
 %! expect = 0.0015238;
 %! assert(int32(res * 10^6),  int32(expect * 10^6));
 function out = euler2(start, til, n, y)
+  X = linspace(start, til, n);
+  h = X(2) - X(1);
+  Y = zeros(1, n);
+  Y(1) = y;
+
+  function r_diff = diff(x, y)
+    r_diff = exp(-2 * x) - 2 * y;
+  end
+
+  for i=1:n - 1
+    f = diff(X(i), Y(i));
+    y_star = Y(i) + h * diff(X(i), Y(i));
+    f_star = diff(X(i + 1), y_star);
+    Y(i + 1) = Y(i) + h * (f + f_star) / 2;
+  end
+
+  out = Y(1, n);
 end
